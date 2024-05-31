d198db52ddd481b4a509f81f2d40564f955c3ff8
GodDraven <1766265384@qq.com>
2024-05-31 02:32:33 +0000

    remove unrelated modules

--- benchmark/fabric-v1.4/four-nodes-docker/init.sh
+++
@@ -1,3 +0,0 @@
-cryptogen generate --output=crypto_config --config=crypto_config.yaml
-configtxgen --configPath=. -profile OrgsOrdererGenesis -outputBlock channel_artifacts/genesis.block
-configtxgen --configPath=. -profile OrgsChannel -outputCreateChannelTx channel_artifacts/channel.tx -channelID rpcchannel
